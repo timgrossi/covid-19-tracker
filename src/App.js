38fc062680a05d3953d1163c6e996e999330cc59
Tim Grossi <timgrossi@users.noreply.github.com>
2021-09-19 17:49:39 -0700

    Finished Header and added InfoBox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import './App.css';
 
+
 function App() {
+  const[countries, setCountries] = useState ([]);
+  const [country, setCountry] = useState('worldwide');
+  
+  
+  // STATE = How to write a variable in REACT //
+  
+  // UseEffect = Runs a piece of code based on a given condition
+  
+  useEffect(() => {
+    // The code inside here will run once when the component loads and not again
+    // async -> send a request, wait for it, then do something with info
+    
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+          
+          setCountries(countries);
+        });
+      };
+      
+      getCountriesData();
+    }, []);
+    
+    const onCountryChange = (event) => {
+      const countryCode = event.target.value;
+      
+      console.log("YOOOOOOOOOO >>>>>>", countryCode);
+      
+      setCountry(countryCode);
+    }
+    
+    /// Header////
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div class="app__header">
+        <h1>COVID-19 TRACKER </h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+
+            {/* Loop through all the countries and show a drop down list of the options */}
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+
+             {/*} <MenuItem value="worldwide">Worldwide</MenuItem>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem> */}
+
+            </Select> 
+        </FormControl>
+      </div>
+
+      <div class="app__stats">
+        {/* InfoBoxs */}
+        {/* InfoBoxs */}
+        {/* InfoBoxs */}
+
+      </div>
+
+      {/* Title and Select Input dropdown field */}
+
+
+      {/* Tabel */}
+      {/* Graph */}
+
+      {/* Map */}
+
     </div>
   );
 }
